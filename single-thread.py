57665c3f41cc812ee2fed20804d9631749d193f7
xztdean <xztdean@gmail.com>
2021-04-14 10:13:52 +0800

    Support HTTP 4xx response

--- single-thread.py
+++ single-thread.py
@@ -15,25 +15,31 @@ def wrong_request_method(method: str) -> bool:
 
 def response_header_200(length, modify_time):
     return b'HTTP/1.1 200 OK\r\nDate: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()).encode()\
-             + b'\r\nContent-Length: ' + str(length).encode() + b'\r\nCache-Control: no-cache\r\nLast-Modified: '\
-             + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime(modify_time)).encode() + b'\r\n\r\n'
+             + b'\r\nConnection: keep-alive\r\nContent-Length: ' + str(length).encode() + b'\r\nCache-Control: '\
+             + b'no-cache\r\nLast-Modified: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT",
+                                                              time.gmtime(modify_time)).encode() + b'\r\n\r\n'
 
 
 def response_header_304():
     return b'HTTP/1.1 304 Not Modified\r\nDate: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()).encode()\
-           + b'\r\nCache-Control: no-cache\r\n\r\n'
+           + b'\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\n\r\n'
 
 
 def response_header_400():
-    pass
+    return b'HTTP/1.1 400 Bad Request\r\nDate: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()).encode() \
+           + b'\r\nConnection: keep-alive\r\nCache-Control: no-cache\r\nContent-Length: 15\r\n' \
+           + b'Content-Type: text/plain; charset=utf-8\r\n\r\n400 Bad Request'
 
 
 def response_header_404():
-    pass
+    return b'HTTP/1.1 404 Not Found\r\nDate: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()).encode() \
+           + b'\r\nConnection: keep-alive\r\nCache-Control: no-cache\r\nContent-Length: 13\r\n'\
+           + b'Content-Type: text/plain; charset=utf-8\r\n\r\n404 Not Found'
 
 
 def response_header_408():
-    pass
+    return b'HTTP/1.1 408 Request Timeout\r\nDate: ' + time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()).encode() \
+           + b'\r\nConnection: close\r\nCache-Control: no-cache\r\n\r\n'
 
 
 def not_modified_since(header, modify_time):
@@ -51,6 +57,7 @@ def process_request(data):
     request = data.decode()
     header_end = request.find('\r\n\r\n')
     if header_end <= 0:
+        print('EMPTY')
         raise BadRequest
     header = request[:header_end].split('\r\n')
     req_info = header[0].split(' ')
@@ -84,6 +91,7 @@ def handle_conn(conn):
             print("enter recv part")
             data = conn.recv(1024)
             print(data)
+            # if len(data) > 0:
             response = process_request(data)
             conn.sendall(response)
             print("sent")
